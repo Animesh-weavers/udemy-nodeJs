47d0a390b1c42c725d65277dec29a861b6d81a32
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-06 19:39:51 +0530

    async way

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,9 +1,17 @@
 const fs = require("fs");
 // console.log(fs);
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
+//Blocking,Sync way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// // console.log(textIn);
 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// console.log(textOut);
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written");
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// // console.log(textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File Written");
+
+//Non-blocking async way
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
+  console.warn(err);
+});
+console.log("Will read file!");
