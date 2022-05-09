0dc1f3a6364cebf0d53d2e5594816502c709b0ee
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-09 18:13:48 +0530

    READING AND WRITING FILES ASYNC

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -10,8 +10,17 @@ const fs = require("fs");
 // console.log("File Written");
 
 //Non-blocking async way
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  console.log(data);
-  console.warn(err);
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("ERROR!!");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written!!");
+      });
+    });
+  });
 });
 console.log("Will read file!");
