8b1865f5b9a5c6c00c5df4a6fca89cdc7fc8fc46
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-11 19:01:40 +0530

    refactoring routes

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -4,22 +4,11 @@ const fs = require('fs');
 const port = 3000;
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.status(400).json({
-//     message: 'Hello from the server side!',
-//     app: 'Natours',
-//   });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint....');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -27,9 +16,9 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   //   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -50,9 +39,9 @@ app.post('/api/v1/tours', (req, res) => {
       console.log(err);
     }
   );
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   //   console.log(req.params);
 
   //whenever we can multiply string to number 1 then string convert to number and vice versa
@@ -71,9 +60,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -87,8 +76,9 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'updated tour..',
     },
   });
-});
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+
+const deleteTour = (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
@@ -100,7 +90,20 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours', getAllTours);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
