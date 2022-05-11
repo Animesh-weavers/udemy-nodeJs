92158322dc46fbbcf6238fc225849fc389c15a9a
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-11 18:19:44 +0530

    create get all tours and single tour api and post create new tour api

---
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const port = 3000;
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.status(400).json({
+//     message: 'Hello from the server side!',
+//     app: 'Natours',
+//   });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint....');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  //   console.log(newTour)
+
+  tours.push(newTour);
+  //   console.log(tours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+      console.log(err);
+    }
+  );
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  //   console.log(req.params);
+
+  //whenever we can multiply string to number 1 then string convert to number and vice versa
+  const id = req.params.id * 1;
+  const tour = tours.filter((el) => el.id === id);
+  if (tour.length<=0) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.patch()
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
