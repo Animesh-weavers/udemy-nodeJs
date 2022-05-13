02f37aaea400f6c1bf45ada8753e9aff51d44940
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-13 18:44:06 +0530

    connect db

--- complete-node-bootcamp-master/4-natours/starter/server.js
+++ complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,7 +1,22 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    // console.log(con.connections);
+    console.log('DB CONNECTIOON SUCCESSFUL');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
