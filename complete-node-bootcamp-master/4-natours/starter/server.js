ca2702c86f4ec7fb3c5c12434e8824d111b00d45
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-13 18:57:03 +0530

    create schema and model

--- complete-node-bootcamp-master/4-natours/starter/server.js
+++ complete-node-bootcamp-master/4-natours/starter/server.js
@@ -18,6 +18,24 @@ mongoose
     console.log('DB CONNECTIOON SUCCESSFUL');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});  //Schema
+
+const Tour=mongoose.model('Tour',tourSchema);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
